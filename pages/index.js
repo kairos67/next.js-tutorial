d8c4b80831f44dd6b21f4eaa8eef2fa7982a3ad7
kairos67 <kairosys@gmail.com>
2022-05-09 19:59:40 +0900

    api in Home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import { Axios } from "axios";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useInsertionEffect } from "react/cjs/react.production.min";
 import { Divider, Header } from "semantic-ui-react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  
+  function getData(){
+    Axios.get(API_URL).then(res=>{
+      console.log(res);
+    });
+  }
+  useEffect(()=>{
+    getData();
+  },[]);
+  
   return (
     <div>
       <Head>
