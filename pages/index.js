cc5d6a4a601e45ffeae980317847bc8f116f255c
kairos67 <kairosys@gmail.com>
2022-05-15 18:46:39 +0900

    commit axios in HOME

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,44 @@
-import Axios  from "axios";
+import Axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 //import { useEffect, useInsertionEffect } from "react/cjs/react.production.min";
 import { Divider, Header } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
-  
-  function getData(){
-    Axios.get(API_URL).then(res=>{
-      console.log(res);
+  const [list, setList] = useState([]);
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    Axios
+    .get(API_URL)
+    .then(res => {
+      //handle success
+      console.log(res.data);
+      setList(res.data)
+    })
+    .catch(err => {
+      // handle error
+      console.log(err);
+    })
+    .then(() => {
+      // always executed
     });
   }
-  useEffect(()=>{
+
+  useEffect(() => {
     getData();
-  },[]);
+  }, []);
 
   return (
     <div>
       <Head>
         <title>Home | Kairos</title>
       </Head>
+      <ItemList list={list} />
       Next.js is running{" "}
     </div>
   );
