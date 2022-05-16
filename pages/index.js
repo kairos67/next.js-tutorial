3f75c6c5d0575f70b1707881af7ea415dc0feec6
kairos67 <kairosys@gmail.com>
2022-05-16 19:12:16 +0900

    commit SSR in HOME

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,32 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 //import { useEffect, useInsertionEffect } from "react/cjs/react.production.min";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 import ItemList from "../src/component/ItemList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   function getData() {
     Axios
-    .get(API_URL)
-    .then(res => {
-      //handle success
-      console.log(res.data);
-      setList(res.data)
-    })
-    .catch(err => {
-      // handle error
-      console.log(err);
-    })
-    .then(() => {
-      // always executed
-    });
+      .get(API_URL)
+      .then(res => {
+        //handle success
+        console.log(res.data);
+        setList(res.data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        // handle error
+        console.log(err);
+      })
+      .then(() => {
+        // always executed
+      });
   }
 
   useEffect(() => {
@@ -37,9 +39,31 @@ export default function Home() {
     <div>
       <Head>
         <title>Home | Kairos</title>
+        <meta name="description" content="Kairos is Home"></meta>
       </Head>
-      <ItemList list={list} />
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" styles={{ paddingTop: 40 }}>
+            베스트상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" styles={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>)}
+      <Divider />
       Next.js is running{" "}
+      <Divider />
     </div>
   );
 }
--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,25 +1,32 @@
-//import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { useRouter } from "next/router";
+import { Menu } from "semantic-ui-react";
 
-export default function Gnb(){
-    const activeItem="home";
-      return (
-      <Menu inverted>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-         // onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-         // onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-        //  onClick={this.handleItemClick}
-        />
-      </Menu>
-    )
+export default function Gnb() {
+  const router = useRouter();
+  let activeItem;
+
+  if (router.pathname === "/") {
+    activeItem = "home";
+  } else if (router.pathname === "/about") {
+    activeItem = "about";
   }
+
+  function goLink(e, data) {
+    if (data.name === "home") {
+      router.push("/");
+    } else if (data.name === "about") {
+      router.push("/about");
+    }
+  }
+
+  return (
+    <Menu inverted>
+      <Menu.Item name="home" active={activeItem === "home"} onClick={goLink} />
+      <Menu.Item
+        name="about"
+        active={activeItem === "about"}
+        onClick={goLink}
+      />
+    </Menu>
+  );
+}
